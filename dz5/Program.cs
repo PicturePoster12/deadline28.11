ebcd34bae13972d019794535a12275c92953e508
Роман Пигуляк <romapiguliak@gmail.com>
2024-11-28 23:12:34 +0300

    Добавьте файлы проекта.

---
+++ dz5/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+public enum Exams
+{
+    English,
+    IT,
+    Physics,
+}
+class Student
+{
+    public string Surname;
+    public string Name;
+    public DateTime BirthDay;
+    public Exams Exam;
+    public int Points;
+
+
+    public Student(string surname, string name, string d, Exams exam, int points)
+    {
+        Surname = surname;
+        Name = name;
+        BirthDay = DateTime.Parse(d);
+        Exam = exam;
+        Points = points;
+    }
+}
+class Program
+{
+    static Dictionary<int, Student> Students()
+    {
+        Dictionary<int, Student> students = new Dictionary<int, Student>()
+            {
+                { 1, new Student( "Гуро", "Руслан", "17.09.2006", Exams.IT, 242) },
+                { 2, new Student( "Балобанова", "Арина", "13.03.2006", Exams.English, 247) },
+                { 3, new Student( "Валеев", "Сергей", "25.08.2006", Exams.Physics, 278) },
+                { 4, new Student( "Габов", "Алексей", "05.07.2006", Exams.IT, 252) },
+                { 5, new Student( "Дорожан", "Андрей", "14.05.2006", Exams.IT, 255) },
+                { 6, new Student( "Лисюткина", "Руслана", "28.06.2006", Exams.IT, 241) },
+                { 7, new Student( "Назарова", "Дарья", "11.05.2006", Exams.English, 239) },
+                { 8, new Student( "Сабирова", "Далия", "14.05.2006", Exams.English, 287) },
+                { 9, new Student( "Спиридонова", "Весна", "15.04.2006", Exams.Physics, 253) },
+                { 10, new Student( "Халикова", "Данна", "03.11.2006", Exams.English, 262) },
+            };
+        return students;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("1.");
+        RandomList();
+    }
+    static void RandomList()
+    {
+        List<string> images = new List<string>
+    {
+        "1.jfif","1.jfif",
+        "2.jfif","2.jfif",
+        "3.jfif","3.jfif",
+        "4.jfif","4.jfif",
+        "5.jfif","5.jfif",
+        "6.jfif","6.jfif",
+        "7.jfif","8.jfif",
+        "8.jfif","8.jfif",
+        "9.jfif","9.jfif",
+        "10.jfif","10.jfif",
+        "11.jfif","11.jfif",
+        "12.jfif","12.jfif",
+        "13.jfif","13.jfif",
+        "14.jfif","14.jfif",
+        "15.jfif","15.jfif",
+        "16.jfif","16.jfif",
+        "17.jfif","17.jfif",
+        "18.jfif","18.jfif",
+        "19.jfif","19.jfif",
+        "20.jfif","20.jfif",
+        "21.jfif","21.jfif",
+        "22.jfif","22.jfif",
+        "23.jfif","23.jfif",
+        "24.jfif","24.jfif",
+        "25.jfif","25.jfif",
+        "26.jfif","26.jfif",
+        "27.jfif","27.jfif",
+        "28.jfif","28.jfif",
+        "29.jfif","29.jfif",
+        "30.jfif","30.jfif",
+        "31.jfif","31.jfif",
+        "32.jfif","32.jfif",
+    };
+        Console.WriteLine("Порядок до перемешивания:");
+        images.ForEach(p => Console.WriteLine(p));
+        Random random = new Random();
+        string temp = images[0];
+        for (int i = 0; i < images.Count; i++)
+        {
+            images.RemoveAt(0);
+            images.Insert(random.Next(images.Count), temp);
+        }
+        Console.WriteLine("Порядок после перемешивания:");
+        images.ForEach(p => Console.WriteLine(p));
+        return;
+    }
+    static void Task2()
+    {
+        Console.WriteLine("2.");
+        Dictionary<int, Student> students = Students();
+        Console.WriteLine("Список:");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key}: {student.Value.Name} {student.Value.Surname} {student.Value.BirthDay} {student.Value.Exam} {student.Value.Points}");
+        }
+        Console.WriteLine("Напишите 'Новый студент', если хотите добавить студента, напишите 'Удалить', если хотите удалить студента, напишите 'Отсортировать', если хотите отсортировать список по баллам");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "Новый студент":
+                students.Add(students.Count + 1, new Student("Пигуляк", "Роман", "27.07.2006", Exams.IT, 243));
+                break;
+            case "Удалить":
+                Console.WriteLine("Введите имя и фамилию студента:");
+                string RemoveName = Console.ReadLine().ToLower();
+                string RemoveSurname = Console.ReadLine().ToLower();
+                students.Remove(students.FirstOrDefault(x => x.Value.Name.ToLower() == RemoveName && x.Value.Surname.ToLower() == RemoveSurname).Key);
+                break;
+            case "Отсортировать":
+                students = students.OrderBy(d  => d.Value.Points).ToDictionary(d =>d.Key, d => d.Value);
+                break;
+        }
+    }
+}
---
+++ tumakov/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+class Laba
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите имя файла:");
+        string filename = Console.ReadLine();
+        Task1(filename);
+        Task2();
+        Task3();
+        Task4(filename);
+        Task6();
+    }
+    static void Task1(string filename)
+    {
+        try {
+            Console.WriteLine("6.1");
+            char[] file = File.ReadAllText(filename).ToCharArray();
+            Console.WriteLine($"Колво гласных: {Glas(file)}");
+            Console.WriteLine($"Колво гласных: {Soglas(file)}");
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка обработки файла");
+        }
+    }
+    static void Task2()
+    {
+        Console.WriteLine("6.2");
+        int[,] matrix1 = MatrixRead();
+        int[,] matrix2 = MatrixRead();
+        MatrixPrint(matrix1);
+        MatrixPrint(matrix2);
+        MatrixPrint(MatrixProizv(matrix1, matrix2));
+    }
+    static void Task3()
+    {
+        Console.WriteLine("6.3");
+        Random random = new Random();
+        int[,] temperature = new int[12, 30];
+        for (int i = 0; i < temperature.GetLength(0); i++)
+        {
+            for (int j = 0; j < temperature.GetLength(1); j++)
+            {
+                temperature[i, j] = random.Next(-100, 100);
+            }
+        }
+        string[] months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+        for (int i = 0; i < months.Length; i++)
+        {
+            Console.WriteLine($"{months[i]}: {AverageTemperature(temperature)[i]}");
+        }
+    }
+    static void Task4(string filename)
+    {
+        Console.WriteLine("dz6.1");
+        List<char> fileList = File.ReadAllText(filename).ToList();
+        Console.WriteLine($"Колво гласных: {Glas(fileList)}");
+        Console.WriteLine($"Колво гласных: {Soglas(fileList)}");
+    }
+    static void Task5()
+    {
+        
+    }
+    static void Task6()
+    {
+        Console.WriteLine("dz6.3");
+        Random random = new Random();
+        Dictionary<string, double[]> tempByMonth = new Dictionary<string, double[]>
+        {
+            {"Январь", TempGenerate(random)},
+            {"Февраль", TempGenerate(random)},
+            {"Март", TempGenerate(random)},
+            {"Апрель", TempGenerate(random)},
+            {"Май", TempGenerate(random)},
+            {"Июнь", TempGenerate(random)},
+            {"Июль", TempGenerate(random)},
+            {"Август", TempGenerate(random)},
+            {"Сентябрь", TempGenerate(random)},
+            {"Октябрь", TempGenerate(random)},
+            {"Ноябрь", TempGenerate(random)},
+            {"Декабрь", TempGenerate(random)}
+        };
+        double[] averTemp = new double[tempByMonth.Count];
+        int index = 0;
+        foreach (var month in tempByMonth.Keys)
+        {
+            double[] dailyTemps = tempByMonth[month];
+            double totalTemp = 0;
+
+            foreach (double temp in dailyTemps)
+            {
+                totalTemp += temp;
+            }
+
+            averTemp[index++] = totalTemp / dailyTemps.Length;
+            Console.WriteLine($"Средняя температура за {month}: {averTemp[index - 1]:F1}");
+        }
+        Array.Sort(averTemp);
+
+        Console.WriteLine("Отсортированные средние температуры по месяцам:");
+        foreach (double avgTemp in averTemp)
+        {
+            Console.WriteLine(avgTemp.ToString("F1"));
+        }
+    }
+    static double[] TempGenerate(Random random)
+    {
+        const int daysInMonth = 30;
+        double[] temp = new double[daysInMonth];
+
+        for (int i = 0; i < daysInMonth; i++)
+        {
+            temp[i] = -10 + (random.NextDouble() * 50);
+        }
+        return temp;
+    }
+    static int Glas(char[] file)
+    {
+        int countGlas = 0;
+        string glas = "аяуюеёиыоэАЯУЮЕЁИЫОЭaeiouyAEIOUY";
+        foreach (char c in file)
+        {
+            if (glas.Contains(c))
+            {
+                countGlas++;
+            }
+        }
+        return countGlas;
+    }
+    static int Soglas(char[] file)
+    {
+        int countSoglas = 0;
+        string soglas = "бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬbcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
+        foreach (char c in file)
+        {
+            if (soglas.Contains(c)) 
+            {
+                countSoglas++;
+            }
+        }
+        return countSoglas;
+    }
+    static int Glas(List<char> list)
+    {
+        int countGlas = 0;
+        string glas = "аяуюеёиыоэАЯУЮЕЁИЫОЭaeiouyAEIOUY";
+        foreach (char c in list)
+        {
+            if (glas.Contains(c))
+            {
+                countGlas++;
+            }
+        }
+        return countGlas;
+    }
+    static int Soglas(List<char> list)
+    {
+        int countSoglas = 0;
+        string soglas = "бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬbcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
+        foreach (char c in list)
+        {
+            if (soglas.Contains(c))
+            {
+                countSoglas++;
+            }
+        }
+        return countSoglas;
+    }
+    static int[,] MatrixRead()
+    {
+            Console.WriteLine("Введите количество строк матрицы:");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов матрицы:");
+            int column = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[row, column];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write($"Введите элемент [{i + 1}, {j + 1}]: ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return matrix;
+    }
+    static void MatrixPrint(int[,] matrix)
+    {
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                Console.Write($"{matrix[i, j]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+    static int[,] MatrixProizv(int[,] matrix1, int[,] matrix2)
+    {
+        int row1 = matrix1.GetLength(0);
+        int column1 = matrix1.GetLength(1);
+        int row2 = matrix2.GetLength(0);
+        int column2 = matrix2.GetLength(1);
+        if (column1 != column2)
+        {
+            throw new ArgumentException();
+        }
+        int[,] result = new int[row1, column2];
+        for (int i = 0; i < row1; i++)
+        {
+            for (int j = 0; j < column2; j++)
+            {
+                for (int k = 0; k < column1; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+        return result;
+    }
+    static int[] AverageTemperature(int[,] temperature)
+    {   
+        int[] averTemp = new int[12];
+        for (int i = 0; i < temperature.GetLength(0);i++)
+        {
+            int dayTemp = 0;
+            for (int j = 0; j < temperature.GetLength(1);j++)
+            {
+                dayTemp += temperature[i, j];
+            }
+            dayTemp /= temperature.GetLength(1);
+            averTemp[i] = dayTemp;
+        }
+        return averTemp;
+    } 
+}
